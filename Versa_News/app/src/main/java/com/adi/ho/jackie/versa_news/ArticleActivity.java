e754beb9713141d93d7533d4471fff25b7934334
Yanni-Psitos <ypsitos@gmail.com>
2016-03-08 12:01:35 -0500

    Twitter Login Button functionality and layout added

---
+++ Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/ArticleActivity.java
@@ -0,0 +1,23 @@
+package com.adi.ho.jackie.versa_news;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class ArticleActivity extends AppCompatActivity {
+
+    ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_article);
+
+        
+        mImageView = (ImageView)findViewById(R.id.imageView);
+        Picasso.with(ArticleActivity.this).load("https://vice-images.vice.com/images/content-images-crops/2016/03/07/the-vice-morning-bulletin-03-07-16-body-image-1457354769-size_1000.jpg").fit().into(mImageView);
+
+    }
+}
--- Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/MainActivity.java
+++ Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/MainActivity.java
@@ -1,13 +1,69 @@
 package com.adi.ho.jackie.versa_news;
 
+import android.app.Application;
+import android.content.Intent;
+import android.service.media.MediaBrowserService;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import io.fabric.sdk.android.Fabric;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TwitterLoginButton loginButton;
+    private static final String TWITTER_KEY = "h8Rnv9R1AdZlL4s1joefXvY3i";
+    private static final String TWITTER_SECRET = "NB0PEP1A7uPk8cxgWZzClz8AQthBSi9UbzE3sfDO0voBWqIiPT";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
+
+//
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+
+            @Override
+            public void success(Result<TwitterSession> result) {
+
+                // The TwitterSession is also available through:
+                TwitterSession session = Twitter.getInstance().core.getSessionManager().getActiveSession();
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+
+            }
+
+            public void failure(TwitterException exception) {
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+        });
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // Make sure that the loginButton hears the result from any
+        // Activity that it triggered.
+        loginButton.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
+
