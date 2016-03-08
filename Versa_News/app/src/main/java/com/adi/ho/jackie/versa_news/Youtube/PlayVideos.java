00bfa5da61f631fd384711e84d59cdc5ea32c437
Rcureton <rashad.cureton@gmail.com>
2016-03-08 14:06:08 -0500

    Completed Youtube API

--- Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/Youtube/PlayVideos.java
+++ Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/Youtube/PlayVideos.java
@@ -1,18 +1,26 @@
 package com.adi.ho.jackie.versa_news.Youtube;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.adi.ho.jackie.versa_news.R;
 import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
 import com.google.api.services.youtube.YouTube;
 import com.google.api.services.youtube.model.Playlist;
 
 import java.util.ArrayList;
 
-public class PlayVideos extends YouTubeBaseActivity {
+public class PlayVideos extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener{
     private YouTube youtube;
     private YouTube.Search.List query;
     private YouTubePlayerView playerView;
@@ -35,13 +43,44 @@ public class PlayVideos extends YouTubeBaseActivity {
         setContentView(R.layout.activity_play_videos);
 
         mVideosList=(ListView)findViewById(R.id.listView);
-        playerView = (YouTubePlayerView)findViewById(R.id.player_view);
+
         mVideos= new ArrayList<>();
 
         LoadYoutubeTask load= new LoadYoutubeTask();
         load.execute();
+//        playerView.initialize(KEY, this);
+
+        mVideosList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("id",mVideos.get(position).getId());
+                Intent intent = YouTubeStandalonePlayer.createPlaylistIntent(PlayVideos.this,
+                        KEY, mVideos.get(position).getId());
+                startActivity(intent);
+
+
+//                Intent intent = new Intent(PlayVideos.this, PlayVideos.class);
+//                intent.putExtra("video", mVideos.get(position).getId());
+            }
+        });
+
 
     }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean restored) {
+        if(!restored){
+            youTubePlayer.cueVideo(getIntent().getStringExtra("video"));
+        }
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        Toast.makeText(this, getString(R.string.failed), Toast.LENGTH_LONG).show();
+    }
+
+
 
     public ArrayList<Playlist> getVideosList(){
         helper= new YoutubeHelper(PlayVideos.this);
@@ -55,6 +94,8 @@ public class PlayVideos extends YouTubeBaseActivity {
             }catch (Throwable throwable){
                 videoItem.setThumbnailURL("http://images.fineartamerica.com/images-medium-large/2-nyc-empire-nina-papiorek.jpg");
             }
+            videoItem.setId(playlist.getId());
+            Log.d("Video",videoItem.getId());
 
             videoItem.setTitle(playlist.getSnippet().getTitle());
             videoItem.setDescription(playlist.getSnippet().getDescription());
@@ -63,6 +104,7 @@ public class PlayVideos extends YouTubeBaseActivity {
     }return mPlayListArrayList;
 
 }
+
 
     public class LoadYoutubeTask extends AsyncTask<Void, Void, ArrayList<Playlist>> {
 
