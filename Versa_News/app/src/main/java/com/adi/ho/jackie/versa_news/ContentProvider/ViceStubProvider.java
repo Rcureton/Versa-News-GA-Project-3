e65e0584bf0d7c2aa9f32414fea9116b1d5d50c9
roberrera <roberterrera@gmail.com>
2016-03-09 16:05:33 -0500

    Add ViceStubProvider.java

---
+++ Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/ContentProvider/ViceStubProvider.java
@@ -0,0 +1,64 @@
+package com.adi.ho.jackie.versa_news.ContentProvider;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+/**
+ * Created by Rob on 3/9/16.
+ */
+public class ViceStubProvider extends ContentProvider {
+    /*
+     * Always return true, indicating that the
+     * provider loaded correctly.
+     */
+    @Override
+    public boolean onCreate() {
+        return true;
+    }
+    /*
+     * Return no type for MIME type
+     */
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+    /*
+     * query() always returns no results
+     *
+     */
+    @Override
+    public Cursor query(
+            Uri uri,
+            String[] projection,
+            String selection,
+            String[] selectionArgs,
+            String sortOrder) {
+        return null;
+    }
+    /*
+     * insert() always returns null (no URI)
+     */
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        return null;
+    }
+    /*
+     * delete() always returns "no rows affected" (0)
+     */
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+        return 0;
+    }
+    /*
+     * update() always returns "no rows affected" (0)
+     */
+    public int update(
+            Uri uri,
+            ContentValues values,
+            String selection,
+            String[] selectionArgs) {
+        return 0;
+    }
+}
