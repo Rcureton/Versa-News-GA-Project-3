be11d6c1740f429c43249e36ddbbc34f1fa36e78
roberrera <roberterrera@gmail.com>
2016-03-09 10:59:49 -0500

    Update SyncAdapter.java and snycadapter.xml

--- Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/SyncAdapter/SyncAdapter.java
+++ Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/SyncAdapter/SyncAdapter.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import com.adi.ho.jackie.versa_news.GSONClasses.ViceDataClass;
 import com.adi.ho.jackie.versa_news.ContentProvider.ViceContentProvider;
 import com.adi.ho.jackie.versa_news.ContentProvider.ViceDBHelper;
+import com.adi.ho.jackie.versa_news.MainActivity;
 import com.google.gson.Gson;
 
 import java.io.BufferedReader;
@@ -41,27 +42,32 @@ public class SyncAdapter extends AbstractThreadedSyncAdapter {
     }
 
     @Override
-    public void onPerformSync(Account account, Bundle extras, String authority, ContentProviderClient provider, SyncResult syncResult) {
+    public void onPerformSync(
+            Account account,
+            Bundle extras,
+            String authority,
+            ContentProviderClient provider,
+            SyncResult syncResult) {
+
         Log.d(SyncAdapter.class.getName(), "Starting sync");
-
         Cursor cursor = mResolver.query(ViceContentProvider.CONTENT_URI,null,null,null,null);
-
-        // TODO: Update the columns and methods here.
-        ViceDataClass viceData;
+/*
+        // TODO: Update the methods here to refresh the data that should be refreshed. The refreshed data should update the database and replace the existing database data.
+        ViceDataClass title;
         while(cursor.moveToNext()){
-            viceData = getData(cursor.getString(cursor.getColumnIndex(ViceDBHelper.COLUMN_STOCK_SYMBOL)));
+            title = getData(cursor.getString(cursor.getColumnIndex(ViceDBHelper.COLUMN_TITLE)));
             Uri uri = Uri.parse(ViceContentProvider.CONTENT_URI + "/" + cursor.getString(cursor.getColumnIndex(ViceDBHelper.COLUMN_ID)));
             ContentValues values = new ContentValues();
-            values.put(ViceDBHelper.COLUMN_STOCK_PRICE, getData().getItems());
+            values.put(ViceDBHelper.COLUMN_BODY, getData(MainActivity.getMostPopularURL).getItems());
             mResolver.update(uri,values,null,null);
         }
-
+*/
     }
 
-    private ViceDataClass getData(String viceData){
+    private ViceDataClass getData(String viceURL){
         String data ="";
         try {
-            URL url = new URL();
+            URL url = new URL(viceURL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.connect();
             InputStream inStream = connection.getInputStream();
