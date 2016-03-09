13c337ddeb0e7d8ef32e45363371745e147ad2c6
Rcureton <rashad.cureton@gmail.com>
2016-03-09 14:52:14 -0500

    Completed Facebook API with Read and Write Permissions

--- Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/MainActivity.java
+++ Versa_News/app/src/main/java/com/adi/ho/jackie/versa_news/MainActivity.java
@@ -2,11 +2,13 @@ package com.adi.ho.jackie.versa_news;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.adi.ho.jackie.versa_news.Facebook.FacebookActivity;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookSdk;
@@ -24,13 +26,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //TODO: I believe this needs to stay on the Activity on the OnCreate
-        FacebookSdk.sdkInitialize(getApplicationContext());
 
         setContentView(R.layout.activity_main);
 
 //
-
+        Button facebook= (Button)findViewById(R.id.facebook);
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(MainActivity.this, FacebookActivity.class);
+                startActivity(intent);
+            }
+        });
 
         Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
